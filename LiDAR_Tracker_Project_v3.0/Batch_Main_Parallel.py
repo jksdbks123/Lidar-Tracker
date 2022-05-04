c780064d2c02f922d72a979be8399f295f3eb3f8
Zhihiu <33596638+jksdbks123@users.noreply.github.com>
2022-05-04 09:28:24 -0700

    Update Batch_Main_Parallel.py
    hahaha

--- LiDAR_Tracker_Project_v3.0/Batch_Main_Parallel.py
+++ LiDAR_Tracker_Project_v3.0/Batch_Main_Parallel.py
@@ -7,9 +7,15 @@ import os
 from p_tqdm import p_umap
 from functools import partial
 
+def run_mot(mot,ref_LLH,ref_xyz):
+        mot.initialization()
+        mot.mot_tracking()
+        save_result(mot.Off_tracking_pool,ref_LLH,ref_xyz,mot.traj_path,mot.missing_thred)
+        print(mot.traj_path)
+
 if __name__ == "__main__":
 
-
+    
     parser = argparse.ArgumentParser(description='This is a program generating trajectories from .pcap files')
     parser.add_argument('-i','--input', help='path that contains .pcap file', required=True)
     parser.add_argument('-o','--output', help='specified output path', required=True)
@@ -23,10 +29,12 @@ if __name__ == "__main__":
     output_traj_path = os.path.join(output_file_path,'Trajectories')
     if not os.path.exists(output_traj_path):
         os.mkdir(output_traj_path)
+    traj_list = os.listdir(output_traj_path)
     pcap_paths = []
-
+    pcap_names = []
     for f in dir_lis:
         if 'pcap' in f.split('.'):
+            pcap_names.append(f)
             pcap_path = os.path.join(input_path,f)
             pcap_paths.append(pcap_path)
 
@@ -43,23 +51,20 @@ if __name__ == "__main__":
         ref_LLH[:,2] += offset
     ref_LLH[:,[0,1]] = ref_LLH[:,[0,1]] * np.pi/180
     ref_LLH[:,2] = ref_LLH[:,2]/3.2808
+    
     with open(config_path) as f:
         params = json.load(f)
 
-    
-    def run_mot(mot,ref_LLH,ref_xyz):
-        mot.initialization()
-        mot.mot_tracking()
-        save_result(mot.Off_tracking_pool,ref_LLH,ref_xyz,mot.traj_path)
-        print(mot.traj_path)
-        
-    mots = []
-    for i,p in enumerate(pcap_paths):
-        f_name = p.split('.')[0].split('\\')[-1] +'.csv'
-        out_path = os.path.join(output_traj_path, f_name)
-        mots.append(MOT(p,out_path,**params,if_vis=False))
-        print(out_path)
-    n_cpu = args.n_cpu
-    print(f'Parallel Processing Begin with {n_cpu} Cpu(s)')
-    p_umap(partial(run_mot,ref_LLH = ref_LLH, ref_xyz = ref_xyz), mots,num_cpus = n_cpu)
+        mots = []
+        for i,p in enumerate(pcap_paths):
+            f_name = pcap_names[i].split('.')[0] + '.csv'
+            if f_name in traj_list:
+                continue
+            out_path = os.path.join(output_traj_path, f_name)
+            mots.append(MOT(p,out_path,**params,if_vis=False))
+            print(out_path)
+            
+        n_cpu = args.n_cpu
+        print(f'Parallel Processing Begin with {n_cpu} Cpu(s)')
+        p_umap(partial(run_mot,ref_LLH = ref_LLH, ref_xyz = ref_xyz), mots,num_cpus = n_cpu)
 
