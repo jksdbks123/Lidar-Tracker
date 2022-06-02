ed23ad5c18ba9d3afeaad80eee822007bcfffb9b
zhihui <zhChen0412@outlook.com>
2022-06-02 08:47:06 -0700

    update

--- LiDAR_Tracker_Project_v3.0/Utils.py
+++ LiDAR_Tracker_Project_v3.0/Utils.py
@@ -589,3 +589,41 @@ def gen_bckmap(aggregated_maps, N, d_thred, d , bck_n):
         for j in range(thred_map.shape[2]):
             thred_map[:,i,j] = get_thred(aggregated_maps[:,i,j],N = N,d_thred = d_thred,d = d,bck_n = bck_n)
     return thred_map
+def calc_d(d):
+    temp = 2.2*10**(-5) * d**2 + 6.3*10**(-4) * d + 3.5*10**(-3)
+    return 2*temp
+
+def get_thred_paper(temp,N = 10,d_thred = 0.1,bck_n = 3):
+    temp = temp.copy()
+    total_sample = len(temp)
+    bck_ds = []
+    bck_portions = []
+    repeat = 0
+    while repeat < N:
+        if len(temp) == 0:
+            break
+        sample = np.random.choice(temp,replace=False)
+        d = calc_d(sample)
+        ind = np.abs(temp - sample) < d
+        portion = ind.sum()/total_sample
+        if portion > d_thred:
+            bck_portions.append(portion)
+            bck_ds.append(sample)
+            temp = temp[~ind]
+        repeat += 1
+        
+    bck_ds = np.array(bck_ds)
+    bck_portions = np.array(bck_portions)
+    arg_ind = np.argsort(bck_portions)[::-1]
+    bck_ds_ = bck_ds[arg_ind[:bck_n]]
+    
+    if len(bck_ds_) <= bck_n:
+        bck_ds_ = np.concatenate([bck_ds_,-d * np.ones(bck_n - len(bck_ds_))])
+    return bck_ds_
+
+def gen_bckmap_paper(aggregated_maps, N, d_thred, bck_n):
+    thred_map = np.zeros((3,32,1800))
+    for i in range(thred_map.shape[1]):
+        for j in range(thred_map.shape[2]):
+            thred_map[:,i,j] = get_thred(aggregated_maps[:,i,j],N = N,d_thred = d_thred,bck_n = bck_n)
+    return thred_map
