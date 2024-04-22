6a7a71e6c0db24539d1ed43d69f18a3088193895
czhui960 <zhChen0412@outlook.com>
2024-04-22 08:31:58 -0700

    update

---
+++ RaspberryPi/DataExtractionTools.py
@@ -0,0 +1,132 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from shapely.geometry import LineString,Point, Polygon, shape
+from shapely.ops import unary_union
+import geopandas as gpd
+import pandas as pd
+import io
+from PIL import Image
+import requests
+from io import BytesIO
+import os
+import pickle
+from tqdm import tqdm
+import dpkt
+# from Utils import get_foreground_point_cloud
+from LiDARBase import parse_one_packet,calc_precise_azimuth,laser_id,Data_order,arg_omega,calc_timing_offsets,get_foreground_point_cloud
+from Utils import *
+from GenBckFile import *
+
+# find the start and end of each consecutive activation event
+def find_concecutive_activation(activation):
+    activation_start = []
+    activation_end = []
+    activation_flag = False
+    for i in range(len(activation)):
+
+        if activation[i] == True and activation_flag == False:
+            activation_start.append(i)
+            activation_flag = True
+        elif activation[i] == False and activation_flag == True:
+            activation_end.append(i)
+            activation_flag = False
+    return activation_start,activation_end
+
+def get_merged_activation(activation_start,activation_end,lane_activation):
+    lane_activation = lane_activation.copy()
+    activation_start,activation_end = find_concecutive_activation(lane_activation)
+    for i in range(1,len(activation_end)):
+        if activation_start[i] - activation_end[i-1] < 6:
+            lane_activation[activation_end[i-1]:activation_start[i]] = True
+    return lane_activation
+
+def load_pcap(file_path):
+    try:
+        fpcap = open(file_path, 'rb')
+        eth_reader = dpkt.pcap.Reader(fpcap)
+    except Exception as ex:
+        print(str(ex))
+        return None
+    return eth_reader
+    
+def read_packets_offline(pcap_file_path):
+    eth_reader = load_pcap(pcap_file_path)
+    while True:
+        # Simulate reading a packet from the Ethernet
+        try:
+            ts,buf = next(eth_reader)
+            eth = dpkt.ethernet.Ethernet(buf)
+        except:
+            # when it's empty, reload current pcap
+            eth_reader = load_pcap(pcap_file_path)
+        if eth.type == 2048: # for ipv4
+            if (type(eth.data.data) == dpkt.udp.UDP):# for ipv4
+                data = eth.data.data.data
+                packet_status = eth.data.data.sport
+                if packet_status == 2368:
+                    if len(data) != 1206:
+                        continue
+            # raw_packet = np.random.rand(20000,2) * 600  # Placeholder for actual packet data
+                    yield (ts,data)
+                    
+def parse_packets(packet_gen):
+    
+    culmulative_azimuth_values = []
+    culmulative_laser_ids = []
+    culmulative_distances = []
+    # culmulative_intensities = []
+    Td_map = np.zeros((32,1800))
+    # Intens_map = np.zeros((32,1800))
+    next_ts = 0
+    
+    ts,raw_packet = next(packet_gen)
+    distances,intensities,azimuth_per_block,Timestamp = parse_one_packet(raw_packet)
+    print(Timestamp)
+    next_ts = Timestamp + 100000 # 0.1sec
+    azimuth = calc_precise_azimuth(azimuth_per_block) # 32 x 12
+    culmulative_azimuth_values.append(azimuth)
+    culmulative_laser_ids.append(laser_id)
+    culmulative_distances.append(distances)
+            
+    
+    while True:
+        while True:
+            ts,raw_packet = next(packet_gen)
+            # Placeholder for parsing logic; here we just pass the data through
+            distances,intensities,azimuth_per_block,Timestamp = parse_one_packet(raw_packet)
+            # flag = self.if_rollover(azimuth_per_block,Initial_azimuth)
+            azimuth = calc_precise_azimuth(azimuth_per_block) # 32 x 12
+            
+            if Timestamp > next_ts:
+                
+                if len(culmulative_azimuth_values) > 0:
+                    
+                    culmulative_azimuth_values = np.concatenate(culmulative_azimuth_values,axis = 1)
+                    culmulative_azimuth_values += Data_order[:,1].reshape(-1,1)
+                    culmulative_laser_ids = np.concatenate(culmulative_laser_ids,axis = 1).flatten()
+                    culmulative_distances = np.concatenate(culmulative_distances,axis = 1).flatten()
+                    # culmulative_intensities = np.concatenate(culmulative_intensities,axis = 1).flatten()
+                    culmulative_azimuth_inds = np.around(culmulative_azimuth_values/0.2).astype('int').flatten()
+                    culmulative_azimuth_inds[(culmulative_azimuth_inds<0)|(culmulative_azimuth_inds>1799)] = culmulative_azimuth_inds[(culmulative_azimuth_inds<0)|(culmulative_azimuth_inds>1799)]%1799
+
+                    Td_map[culmulative_laser_ids,culmulative_azimuth_inds] = culmulative_distances
+                    # Intens_map[culmulative_laser_ids,culmulative_azimuth_inds] = culmulative_intensities
+                    
+                    yield Td_map[arg_omega,:] #32*1800
+                else:
+                    yield Td_map #32*1800
+
+                culmulative_azimuth_values = []
+                culmulative_laser_ids = []
+                culmulative_distances = []
+                # culmulative_intensities = []
+
+                Td_map = np.zeros((32,1800))
+                # Intens_map = np.zeros((32,1800))
+                next_ts += 100000
+                break
+            else:
+                culmulative_azimuth_values.append(azimuth)
+                culmulative_laser_ids.append(laser_id)
+                culmulative_distances.append(distances)
+                # culmulative_intensities.append(intensities)
