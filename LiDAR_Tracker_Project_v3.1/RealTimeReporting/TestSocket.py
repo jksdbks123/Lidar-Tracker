881427ab2cb931bdebf9982204a961bc11570e8e
czhui960 <zhChen0412@outlook.com>
2025-03-07 19:16:30 -0800

    a

--- LiDAR_Tracker_Project_v3.1/RealTimeReporting/TestSocket.py
+++ LiDAR_Tracker_Project_v3.1/RealTimeReporting/TestSocket.py
@@ -7,7 +7,7 @@ import threading
 if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, # Internet
                     socket.SOCK_DGRAM) # UDP
-    sock.bind(('', 2380))     
+    sock.bind(('', 2390))     
     while True:
         data,addr = sock.recvfrom(1206)
         # raw_data_queue.put_nowait((time.time(),data))
