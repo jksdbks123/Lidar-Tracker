1b61313de44693ff879c6d2831b74b0f78749c75
czhui960 <zhChen0412@outlook.com>
2025-03-12 09:36:10 -0700

    a

--- LiDAR_Tracker_Project_v3.1/RealTimeReporting/Main.py
+++ LiDAR_Tracker_Project_v3.1/RealTimeReporting/Main.py
@@ -15,14 +15,14 @@ sys.path.insert(0, interface_path)
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 # Add Interface to sys.path
 sys.path.insert(0, root_path)
-rasp_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', r'RaspberryPi'))
+# rasp_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', r'RaspberryPi'))
 # Add Interface to sys.path
-sys.path.insert(0, rasp_path)
+# sys.path.insert(0, rasp_path)
 print(sys.path)
-# from Utils.LiDARBase import *
-from RaspberryPi.LiDARBase import * 
+from Utils.LiDARBase import *
+# from RaspberryPi.LiDARBase import * 
 from Utils.config import Config
-# from RaspberryPi.LiDARBase import parse_packets,track_point_clouds,read_packets_online
+from RaspberryPi.LiDARBase import parse_packets,track_point_clouds,read_packets_online
 from RaspberryPi.MOT_TD_BCKONLIONE import MOT
 from RaspberryPi.Utils import BarDrawer,line_segments_intersect
 from RaspberryPi.GenBckFile import gen_bckmap
