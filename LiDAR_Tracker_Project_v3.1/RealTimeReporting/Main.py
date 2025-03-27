00ebe2a77a748bc5c5bd0a3ce12de7003ec291e0
czhui960 <zhChen0412@outlook.com>
2025-03-26 18:05:08 -0700

    aa

--- LiDAR_Tracker_Project_v3.1/RealTimeReporting/Main.py
+++ LiDAR_Tracker_Project_v3.1/RealTimeReporting/Main.py
@@ -297,9 +297,9 @@ def run_processes(manager, raw_data_queue, point_cloud_queue, background_point_c
 
         # Shared dictionary for thred_map updates
         thred_map_dict = manager.dict({"thred_map": initial_thred_map})
-        tracking_pool_dict = manager.dict({})
-        off_tracking_pool_dict = manager.dict({})
-        
+        tracking_pool_dict = manager.dict()
+        off_tracking_pool_dict = manager.dict()
+
         mot = MOT(tracking_parameter_dict, thred_map_dict["thred_map"], missing_thred=10,Tracking_pool = tracking_pool_dict,Off_tracking_pool = off_tracking_pool_dict)
         # Creating processes
         packet_reader_process = multiprocessing.Process(target=read_packets_online, name= 'ReadPacket', args=(port, raw_data_queue,))
