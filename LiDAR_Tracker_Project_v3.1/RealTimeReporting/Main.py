03b975868200de12a0e6b320449c1d7a9c1c025a
czhui960 <zhChen0412@outlook.com>
2025-03-31 22:59:13 -0700

    update

--- LiDAR_Tracker_Project_v3.1/RealTimeReporting/Main.py
+++ LiDAR_Tracker_Project_v3.1/RealTimeReporting/Main.py
@@ -371,7 +371,7 @@ if __name__ == "__main__":
     port = 2380
     mode = "online" 
     data_reporting_interval = 1 # min
-    background_data_generting_time = 160 # sec used frames to generate background
+    background_data_generting_time = 120 # sec used frames to generate background
     background_update_interval = 360 # sec
     with multiprocessing.Manager() as manager:
         # Define queues **once** and reuse them
